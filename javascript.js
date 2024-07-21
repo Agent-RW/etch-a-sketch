5ba145b0b0733db33a79037e53558c04c74ae45e
Ronald W <RonWingate11@gmail.com>
2024-07-21 18:50:12 -0400

    feat: Enable user to reset sketch grid with Reset button

--- javascript.js
+++ javascript.js
@@ -1,28 +1,49 @@
 const SKETCH_PAD = document.querySelector("#sketch-pad");
 const RESET_BTN = document.querySelector("#reset-button");
-const GRID_ROWS = 16;
-const GRID_COLS = 16;
-let tileSize = (1 / GRID_ROWS) * SKETCH_PAD.offsetHeight;
+let rows = 16;
+let cols = 16;
+let tileSize;
 
-for (let row = 0; row < GRID_ROWS; row++) {
-  let gridRow = document.createElement("div");
+function newGrid(rows, cols) {
+  tileSize = (1 / rows) * SKETCH_PAD.offsetHeight;
+  for (let row = 0; row < rows; row++) {
+    let gridRow = document.createElement("div");
 
-  gridRow.style.height = `${tileSize}px`;
-  gridRow.style.display = "flex";
+    gridRow.style.height = `${tileSize}px`;
+    gridRow.style.display = "flex";
 
-  for (let col = 0; col < GRID_COLS; col++) {
-    let gridTile = document.createElement("div");
-    gridTile.addEventListener("mouseenter", (e) => {
-      e.target.style.backgroundColor = "red";
-    });
-    gridTile.addEventListener("mouseleave", (e) => {
-      e.target.style.backgroundColor = "green";
-    });
+    for (let col = 0; col < cols; col++) {
+      let gridTile = document.createElement("div");
+      gridTile.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "red";
+      });
+      gridTile.addEventListener("mouseleave", (e) => {
+        e.target.style.backgroundColor = "green";
+      });
 
-    gridTile.style.backgroundColor = "blue";
-    gridTile.style.width = `${tileSize}px`;
-    gridRow.appendChild(gridTile);
+      gridTile.style.backgroundColor = "blue";
+      gridTile.style.width = `${tileSize}px`;
+      gridRow.appendChild(gridTile);
+    }
+
+    SKETCH_PAD.appendChild(gridRow);
+  }
+}
+
+function setupControls() {
+  RESET_BTN.addEventListener("click", onReset);
+}
+
+function onReset(event) {
+  while (SKETCH_PAD.firstChild) {
+    SKETCH_PAD.removeChild(SKETCH_PAD.firstChild);
   }
 
-  SKETCH_PAD.appendChild(gridRow);
+  let newRows = prompt("How many rows?");
+  let newCols = prompt("How many columns?");
+
+  newGrid(newRows, newCols);
 }
+
+newGrid(rows, cols);
+setupControls();
